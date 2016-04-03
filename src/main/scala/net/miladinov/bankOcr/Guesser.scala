dcd4c019151e9401e243a5cef39413e72088a42f
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-04-02 22:41:44 -0400

    Further extract duplicated code

--- src/main/scala/net/miladinov/bankOcr/Guesser.scala
+++ src/main/scala/net/miladinov/bankOcr/Guesser.scala
@@ -5,7 +5,7 @@ object Guesser {
   def additions (g: String): Set[String] = additions(g.split("\n"))
 
   def additions (glyph: IndexedSeq[String]): Set[String] = {
-    val transforms: List[IndexedSeq[String] => IndexedSeq[String]] = List(
+    performTransformations(List(
       changeTop("_"),
       changeMidLeft("|"),
       changeMidMid("_"),
@@ -13,19 +13,13 @@ object Guesser {
       changeBotLeft("|"),
       changeBotMid("_"),
       changeBotRight("|")
-    )
-
-    transforms
-      .map(x => (glyph, x(glyph)))
-      .filterNot { case (a, b) => a == b }
-      .map { case (a, b) => b.mkString("\n") }
-      .toSet
+    ), glyph)
   }
 
   def removals (g: String): Set[String] = removals(g.split("\n"))
 
   def removals (glyph: IndexedSeq[String]): Set[String] = {
-    val transforms: List[IndexedSeq[String] => IndexedSeq[String]] = List(
+    performTransformations(List(
       changeTop(" "),
       changeMidLeft(" "),
       changeMidMid(" "),
@@ -33,8 +27,12 @@ object Guesser {
       changeBotLeft(" "),
       changeBotMid(" "),
       changeBotRight(" ")
-    )
+    ), glyph)
+  }
 
+  type StringTransformer = IndexedSeq[String] => IndexedSeq[String]
+
+  private def performTransformations (transforms: List[StringTransformer], glyph: IndexedSeq[String]): Set[String] = {
     transforms
       .map(x => (glyph, x(glyph)))
       .filterNot { case (a, b) => a == b }
@@ -42,43 +40,43 @@ object Guesser {
       .toSet
   }
 
-  private def changeTop (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeTop (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(l, _, r) = top.split("")
     IndexedSeq(IndexedSeq(l, c, r).mkString, mid, bot)
   }
 
-  private def changeMidLeft (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeMidLeft (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(_, m, r) = mid.split("")
     IndexedSeq(top, IndexedSeq(c, m, r).mkString, bot)
   }
 
-  private def changeMidMid (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeMidMid (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(l, _, r) = mid.split("")
     IndexedSeq(top, IndexedSeq(l, c, r).mkString, bot)
   }
 
-  private def changeMidRight (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeMidRight (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(l, m, _) = mid.split("")
     IndexedSeq(top, IndexedSeq(l, m, c).mkString, bot)
   }
 
-  private def changeBotLeft (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeBotLeft (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(_, m, r) = bot.split("")
     IndexedSeq(top, mid, IndexedSeq(c, m, r).mkString)
   }
 
-  private def changeBotMid (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeBotMid (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(l, _, r) = bot.split("")
     IndexedSeq(top, mid, IndexedSeq(l, c, r).mkString)
   }
 
-  private def changeBotRight (c: String): (IndexedSeq[String]) => IndexedSeq[String] = g => {
+  private def changeBotRight (c: String): StringTransformer = g => {
     val IndexedSeq(top, mid, bot) = g
     val Array(l, m, _) = bot.split("")
     IndexedSeq(top, mid, IndexedSeq(l, m, c).mkString)
